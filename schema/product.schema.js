f981687c8faea827f096ed4272527584783eb61c
Kevinr2 <kevinreve096@gmail.com>
2021-11-08 13:53:54 -0500

    despliegue1

---
+++ schema/product.schema.js
@@ -0,0 +1,21 @@
+const joi = require('joi')
+
+const id = joi.string().uuid()
+const name = joi.string().min(3).max(15)
+const price =joi.number().integer().min(10)
+const image = joi.string().uri()
+const createProductShema = joi.object({
+    name:name.required(),
+    price:price.required(),
+    image:image.required()
+})
+const updateProductShema = joi.object({
+    name:name,
+    price:price,
+    image:image
+})
+const getProductShema = joi.object({
+    id:id.required(),
+})
+
+module.exports = { createProductShema, updateProductShema, getProductShema }

951b1c80c4aa386e90d8f75203be501333bfa54b
Kevinr2 <kevinreve096@gmail.com>
2021-11-08 15:03:46 -0500

    despleigue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const  express = require('express')
  const whitelist = ['http://localhost:80','http://localhost:8080', 'http://post.test']//caracteristicas cors // son los mismos (si pego los links platzi me impide comentar)
  const options = {
    origin: (origin, callback) => {
-     if (whitelist.includes(origin)) {
+     if (whitelist.includes(origin) || !origin) {
        callback(null, true);
      } else {
        callback(new Error('no permitido'), false);
